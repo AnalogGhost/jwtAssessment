7ce18376cf50e28c02e1620d27c4842273642377
Sam Cate <SamCate@Sams-MacBook-Pro.local>
2016-08-10 11:04:10 -0600

    Working jwt

--- server.js
+++ server.js
@@ -10,11 +10,23 @@ app.use(express.static('public'));
 
 app.get('/login',function (req,res,next) {
   //TODO: Return a token
+  var user = {
+    name: 'Sam'
+  }
+  // console.log(localStorage)
+  res.json({token:jwt.sign(user,process.env.SECRET)});
 });
 
 app.use(function (req,res,next) {
   //TODO: Implement app level middleware to protect the /protected route
   //TODO: Verify the token before allowing access to /protected
+  jwt.verify(req.token, process.env.SECRET, function(err, decoded) {
+    if (!err) {
+      next();
+    } else {
+      res.status(400).send('Bad request');
+    }
+  })
 });
 
 app.get('/protected',function (req,res,next) {
